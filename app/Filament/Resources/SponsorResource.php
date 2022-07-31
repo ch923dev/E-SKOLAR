0b8e00ea5231fa72fa2abeacc4098b398e65103f
ch923dev <101375633+ch923dev@users.noreply.github.com>
2022-07-31 15:17:47 +0800

    README UPDATE

---
+++ app/Filament/Resources/SponsorResource.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\SponsorResource\Pages;
+use App\Filament\Resources\SponsorResource\RelationManagers;
+use App\Models\Sponsor;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class SponsorResource extends Resource
+{
+    protected static ?string $model = Sponsor::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('sponsor_category_id')
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name'),
+                Tables\Columns\TextColumn::make('sponsor_category_id'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+    
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListSponsors::route('/'),
+            'create' => Pages\CreateSponsor::route('/create'),
+            'view' => Pages\ViewSponsor::route('/{record}'),
+            'edit' => Pages\EditSponsor::route('/{record}/edit'),
+        ];
+    }    
+}
