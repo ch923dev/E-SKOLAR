1ce07c1bf535b2fb93fb55213ac35008a78b1629
ch923dev <101375633+ch923dev@users.noreply.github.com>
2022-11-26 13:25:32 +0800

    Sponsor Resource Added

--- app/Filament/Resources/SponsorResource.php
+++ app/Filament/Resources/SponsorResource.php
@@ -10,27 +10,20 @@ use Filament\Resources\Form;
 use Filament\Resources\Resource;
 use Filament\Resources\Table;
 use Filament\Tables;
-use Filament\Tables\Filters\MultiSelectFilter;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class SponsorResource extends Resource
 {
     protected static ?string $model = Sponsor::class;
-    protected static ?string $modelLabel = 'Sponsor';
-    protected static ?string $navigationIcon = 'fas-handshake-simple';
-    protected static ?string $navigationGroup = 'Sponsors Management';
-    protected static ?int $navigationSort = 1;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                Forms\Components\TextInput::make('name')
-                    ->required(),
-                Forms\Components\Select::make('sponsor_category_id')
-                    ->relationship('sponsor_category', 'name')
-                    ->required()
+                //
             ]);
     }
 
@@ -38,47 +31,25 @@ class SponsorResource extends Resource
     {
         return $table
             ->columns([
-                Tables\Columns\TextColumn::make('name')
-                    ->searchable(),
-                Tables\Columns\TextColumn::make('sponsor_category.name')
-                    ->label('Sponsor Category')
-                    ->sortable()
-                    ->limit(30),
+                //
             ])
             ->filters([
-                MultiSelectFilter::make('sponsor_category')
-                    ->relationship('sponsor_category', 'name')
-                    ->label('Sponsor Category')
-                    ->column('sponsor_category.name')
+                //
             ])
             ->actions([
-                Tables\Actions\ActionGroup::make(
-                    [
-                        Tables\Actions\ViewAction::make(),
-                        Tables\Actions\EditAction::make(),
-                        Tables\Actions\DeleteAction::make(),
-                    ]
-                ),
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\DeleteBulkAction::make(),
             ]);
     }
-
-    public static function getRelations(): array
-    {
-        return [
-            //
-        ];
-    }
-
+    
     public static function getPages(): array
     {
         return [
-            'index' => Pages\ListSponsors::route('/'),
-            'create' => Pages\CreateSponsor::route('/create'),
-            'view' => Pages\ViewSponsor::route('/{record}'),
-            'edit' => Pages\EditSponsor::route('/{record}/edit'),
+            'index' => Pages\ManageSponsors::route('/'),
         ];
-    }
+    }    
 }
