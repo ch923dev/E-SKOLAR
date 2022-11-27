328d18af93c19cf033c07885e4a0f45535e0aaf7
ch923dev <101375633+ch923dev@users.noreply.github.com>
2022-11-27 11:30:25 +0800

    Sponsor Resource

--- app/Filament/Resources/SponsorResource/Pages/ManageSponsors.php
+++ app/Filament/Resources/SponsorResource/Pages/ManageSponsors.php
@@ -3,6 +3,9 @@
 namespace App\Filament\Resources\SponsorResource\Pages;
 
 use App\Filament\Resources\SponsorResource;
+use Filament\Forms\Components\Repeater;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Wizard\Step;
 use Filament\Pages\Actions;
 use Filament\Resources\Pages\ManageRecords;
 
@@ -13,7 +16,26 @@ class ManageSponsors extends ManageRecords
     protected function getActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            Actions\CreateAction::make()
+                ->steps([
+                    Step::make('Sponsor')
+                        ->schema([
+                            TextInput::make('sponsor')
+                                ->label('Sponsor Name')
+                                ->required(),
+                        ]),
+                    Step::make('Scholarship Program')
+                        ->schema([
+                            Repeater::make('scholarship_programs')
+                                ->label('Scholarship Programs')
+                                ->relationship('scholarship_programs')
+                                ->schema([
+                                    TextInput::make('name')
+                                        ->label('Sponsor')
+                                        ->required()
+                                ])
+                        ])
+                ])
         ];
     }
 }
---
+++ app/Filament/Resources/SponsorResource/RelationManagers/ScholarshipProgramsRelationManager.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Filament\Resources\SponsorResource\RelationManagers;
+
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Resources\Table;
+use Filament\Tables;
+class ScholarshipProgramsRelationManager extends RelationManager
+{
+    protected static string $relationship = 'scholarship_programs';
+
+    protected static ?string $modelLabel = 'Scholarship Program';
+
+    protected static ?string $recordTitleAttribute = 'name';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                Tables\Actions\CreateAction::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+}
