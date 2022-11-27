6033e16d9cdb86512d7368ce5ad5434ed21d01d9
ch923dev <101375633+ch923dev@users.noreply.github.com>
2022-11-27 10:11:48 +0800

    Composer update

--- app/Filament/Resources/SponsorResource.php
+++ app/Filament/Resources/SponsorResource.php
@@ -6,24 +6,28 @@ use App\Filament\Resources\SponsorResource\Pages;
 use App\Filament\Resources\SponsorResource\RelationManagers;
 use App\Models\Sponsor;
 use Filament\Forms;
+use Filament\Forms\Components\Repeater;
+use Filament\Forms\Components\TextInput;
 use Filament\Resources\Form;
 use Filament\Resources\Resource;
 use Filament\Resources\Table;
 use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class SponsorResource extends Resource
 {
     protected static ?string $model = Sponsor::class;
-
-    protected static ?string $navigationIcon = 'heroicon-o-collection';
+    protected static ?string $navigationGroup = 'Scholarships Management';
+    protected static ?string $navigationIcon = 'fas-handshake-angle';
+    protected static ?string $modelLabel = 'Sponsor';
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                //
+                TextInput::make('sponsor')
             ]);
     }
 
@@ -31,7 +35,12 @@ class SponsorResource extends Resource
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('sponsor')
+                    ->label('Sponsor Name')
+                    ->searchable(),
+                TextColumn::make('scholarship_programs_count')
+                    ->counts('scholarship_programs')
+                    ->label('Total Scholarship Program')
             ])
             ->filters([
                 //
@@ -45,11 +54,17 @@ class SponsorResource extends Resource
                 Tables\Actions\DeleteBulkAction::make(),
             ]);
     }
-    
+    public static function getRelations(): array
+    {
+        return [
+            RelationManagers\ScholarshipProgramsRelationManager::class
+        ];
+    }
     public static function getPages(): array
     {
         return [
             'index' => Pages\ManageSponsors::route('/'),
+            'view' => Pages\ViewSponsor::route('/{record}'),
         ];
-    }    
+    }
 }
