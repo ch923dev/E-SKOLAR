b52495a1b6f850416523762a33a8226141d8517b
ch923dev <101375633+ch923dev@users.noreply.github.com>
2022-11-29 18:37:24 +0800

    Added Baranggay Module

---
+++ database/migrations/2022_11_29_072941_create_scholars_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\Baranggay;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('scholars', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class);
+            $table->integer('status')->comment('1. Pending, 2. Inactive 3. Active 4. Graduate');
+            $table->foreignIdFor(Baranggay::class);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('scholars');
+    }
+};
