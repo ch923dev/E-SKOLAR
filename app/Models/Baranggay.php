d9dcc4b4907084754b370165f07a358c8f4211e4
ch923dev <101375633+ch923dev@users.noreply.github.com>
2022-11-29 18:58:08 +0800

    Updated Baranggay Model

--- app/Models/Baranggay.php
+++ app/Models/Baranggay.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Baranggay extends Model
 {
@@ -22,4 +23,9 @@ class Baranggay extends Model
      * @var bool
      */
     public $timestamps = false;
+
+    public function users(): HasMany
+    {
+        return $this->hasMany(User::class);
+    }
 }
