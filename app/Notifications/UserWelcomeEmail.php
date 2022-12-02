056294a705b279ad508363abbab5ad593cae284a
ch923dev <101375633+ch923dev@users.noreply.github.com>
2022-12-02 20:41:42 +0800

    User Email

--- app/Notifications/UserWelcomeEmail.php
+++ app/Notifications/UserWelcomeEmail.php
@@ -2,10 +2,12 @@
 
 namespace App\Notifications;
 
+use App\Models\Role;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
+use Illuminate\Support\Str;
 
 class UserWelcomeEmail extends Notification
 {
@@ -40,10 +42,14 @@ class UserWelcomeEmail extends Notification
      */
     public function toMail($notifiable)
     {
+        $user = $notifiable;
         return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
-                    ->line('Thank you for using our application!');
+            ->greeting('Hello ' . Str::before($user->name, ' ') . ',')
+            ->line('The administrator has created an account for you. This email includes your account details. Login to your account using the credentials below, so please keep it safe.')
+            ->line('Email: ' . $user->email)
+            ->line('Password: ' . Str::slug(Str::before($user->email, '@') . ' ' . Role::find($user->role_id)->role))
+            ->action('Login using this link', url('/'))
+            ->line('Thank you for using our application!');
     }
 
     /**
--- resources/views/email/user/welcome.blade.php
+++
@@ -1,9 +0,0 @@
-<x-mail::message>
-
-    <x-mail::button :url="''">
-        Button Text
-    </x-mail::button>
-
-    Thanks,<br>
-    {{ config('app.name') }}
-</x-mail::message>
