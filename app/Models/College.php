79531cfe45ec50be2a55c9a353dd6af809946df3
ch923dev <101375633+ch923dev@users.noreply.github.com>
2022-12-03 07:44:26 +0800

    Added College Resource

--- app/Models/College.php
+++ app/Models/College.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 
 class College extends Model
 {
@@ -24,5 +25,8 @@ class College extends Model
 
     public function programs() : HasMany{
         return $this->hasMany(Program::class);
+    }
+    public function scholars() : HasManyThrough{
+        return $this->hasManyThrough(Scholar::class,Program::class);
     }
 }
